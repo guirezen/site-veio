73cbd8126d04c4a5c415949de4bd83bc307fb07e
luiz.paes <guirzn@gmail.com>
2024-07-04 14:51:31 -0300

    Adiciona algumas bibliotecas, monta a estrutura organizacional inicial das pastas do projeto.

---
+++ src/Router/index.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export default function RoutesApp() {
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
